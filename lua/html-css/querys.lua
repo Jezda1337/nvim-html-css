3f87b362f9440fb9cb051435cef4f342b7620c42
Radoje <megasrbin1@gmail.com>
2024-12-07 23:26:51 +0100

    add new query

--- lua/html-css/querys.lua
+++ lua/html-css/querys.lua
@@ -23,11 +23,48 @@ M.jsx_link_href = [[
 				(string_fragment) @href_val)))
 ]]
 
+-- M.selectors = [[
+-- 	(id_selector
+-- 		(id_name) @id_name)
+-- 	(class_selector
+-- 		(class_name) @class_name)
+-- ]]
+
 M.selectors = [[
-	(id_selector
-		(id_name) @id_name)
-	(class_selector
-		(class_name) @class_name)
+(
+ (rule_set
+	 (selectors
+		 (class_selector
+			 (class_name) @class_name)
+		 ) @class_decl
+	 (#lua-match? @class_decl "^[.][a-zA-Z0-9_-]+$")
+	 (#not-has-ancestor? @class_decl "media_statement")
+	 (block) @class_block
+	 )
+ )
+;; find nested class
+;; (
+;; (rule_set
+;;	 (selectors
+;;		 (class_selector
+;;			 (class_name) @nested.class_name)
+;;		 ) @nested.class_decl
+;;	 (#lua-match? @nested.class_decl "^[.][a-zA-Z0-9_-]+$")
+;;	 (#not-has-ancestor? @nested.class_decl "media_statement")
+;;	 (block) @nested.class_block
+;;	 ) @nested.rs (#has-ancestor? @nested.rs "rule_set")
+;;)
+(
+ (rule_set
+	 (selectors
+		 (id_selector
+			 (id_name) @id_name)
+		 ) @id_decl
+	 (#lua-match? @id_decl "^#[a-zA-Z0-9_-]+")
+	 (#not-has-ancestor? @id_decl "media_statement")
+	 (block) @id_block
+	 )
+ )
 ]]
 
 M.internal_selectors = [[
