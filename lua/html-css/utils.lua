baec998f75ab6e66b08e4fb7733ce01f40aca6dd
Radoje <megasrbin1@gmail.com>
2025-02-26 23:40:49 +0100

    add couple old utilitiy funcs

--- lua/html-css/utils.lua
+++ lua/html-css/utils.lua
@@ -2,6 +2,27 @@ local uv = vim.uv
 local utils = {}
 
 ---@param path string
+---@return string
+utils.get_source_name = function(path)
+	if utils.is_remote(path) then
+		return path:match("([^/]+)%.css$") or path:match("([^/]+)/$") or "remote"
+	end
+	return vim.fn.fnamemodify(path, ":t:r")
+end
+
+---@param lang string
+---@param langs table<string>
+utils.is_lang_enabled = function(lang, langs)
+	langs = langs or {}
+	for _, v in ipairs(langs) do
+		if v == lang then
+			return true
+		end
+	end
+	return false
+end
+
+---@param path string
 utils.is_remote = function(path)
 	return path:match("^https?://") ~= nil
 end
