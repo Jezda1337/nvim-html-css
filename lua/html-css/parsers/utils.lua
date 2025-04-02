3161cdbf593277852c5679f05d3aea21edc2d211
Radoje <megasrbin1@gmail.com>
2025-04-02 02:08:08 +0200

    migrate parser to 0.11

--- lua/html-css/parsers/utils.lua
+++ lua/html-css/parsers/utils.lua
@@ -1,23 +1,29 @@
 local utils = {}
 
+-- TODO string_parser should not be changed no 0.11 changes to the string parser
+-- rest need some cleanup
+
 ---@type fun(lang: string, qs: string, bufnr: integer): TSNode, vim.treesitter.Query
 utils.parse = function(lang, qs, bufnr)
-	local ts = vim.treesitter
+    local ts = vim.treesitter
 
-	local root = ts.get_parser(bufnr, lang):parse()[1]:root()
-	local query = ts.query.parse(lang, qs)
+    local p = ts.get_parser(bufnr, lang)
+    p:parse() -- this is needed since 0.11
 
-	return root, query
+    local root = p:trees()[1]:root()
+    local query = ts.query.parse(lang, qs)
+
+    return root, query
 end
 
 ---@type fun(lang: string, qs: string, s: string): TSNode, vim.treesitter.Query
 utils.string_parse = function(lang, qs, s)
-	local ts = vim.treesitter
+    local ts = vim.treesitter
 
-	local root = ts.get_string_parser(s, lang):parse()[1]:root()
-	local query = ts.query.parse(lang, qs)
+    local root = ts.get_string_parser(s, lang):parse()[1]:root()
+    local query = ts.query.parse(lang, qs)
 
-	return root, query
+    return root, query
 end
 
 return utils
