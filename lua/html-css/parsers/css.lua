9d5fe0b9a4180b960ef92a2cfe8534f3e2c4f957
Radoje <megasrbin1@gmail.com>
2025-11-25 20:08:14 +0100

    fix: parse classes with dath (-)
    close #61

--- lua/html-css/parsers/css.lua
+++ lua/html-css/parsers/css.lua
@@ -3,49 +3,52 @@ local css = {}
 local ts = vim.treesitter
 
 css.lang = "css"
-css.query = [[
-((rule_set
-	 (selectors
-		 (class_selector
-			 (class_name) @class_name)
-		 ) @class_decl
-	 (#lua-match? @class_decl "^[.][a-zA-Z0-9_-]+$")
-	 (#not-has-ancestor? @class_decl "media_statement")
-	 (block) @class_block
-	 ))
+css.query = [[(class_selector
+  (class_name) @class_name)
+(#not-has-ancestor? @class_name "media_statement")
 
- ((rule_set
-	 (selectors
-		 (id_selector
-			 (id_name) @id_name)
-		 ) @id_decl
-	 (#lua-match? @id_decl "^#[a-zA-Z0-9_-]+")
-	 (#not-has-ancestor? @id_decl "media_statement")
-	 (block) @id_block
-	 ))
+; 2. Extract all IDs anywhere
+(id_selector
+  (id_name) @id_name)
+(#not-has-ancestor? @id_name "media_statement")
 
+; 3. Extract imports (kept as is)
 ((stylesheet
    (import_statement
-	 (string_value
-		(_) @value)
+     (string_value
+        (_) @value)
         (#not-lua-match? @value "^tailwind")
         (#not-lua-match? @value "^tw%-")
 )))
 ((stylesheet
    (import_statement
-	 (call_expression
-	   (function_name)
-	   (arguments
-		 (string_value
-			(_)@value)
+     (call_expression
+       (function_name)
+       (arguments
+         (string_value
+            (_)@value)
         (#not-lua-match? @value "^tailwind")
         (#not-lua-match? @value "^tw%-")
 )))))
 ]]
 
----@param stdout string
----@param withLocation boolean Flag for ignoring remote styles
----@return CSS_Data
+---@param node TSNode
+---@return string
+local function get_rule_block_text(node, source)
+    local current = node
+    while current do
+        if current:type() == "rule_set" then
+            for child in current:iter_children() do
+                if child:type() == "block" then
+                    return ts.get_node_text(child, source)
+                end
+            end
+        end
+        current = current:parent()
+    end
+    return ""
+end
+
 css.setup = function(stdout, withLocation)
     local root, query = utils.string_parse(css.lang, css.query, stdout)
     ---@type CSS_Data
@@ -55,39 +58,44 @@ css.setup = function(stdout, withLocation)
         id = {},
     }
 
-    for _, match, _ in query:iter_matches(root, stdout, 0, -1, { all = true }) do
-        for id, nodes in pairs(match) do
-            local name = query.captures[id]
-            for _, node in ipairs(nodes) do
-                local start_row, start_col, end_row, end_col = node:range()
-                if name == "class_name" then
-                    table.insert(css_data.class, {
-                        type = "class",
-                        label = ts.get_node_text(node, stdout),
-                        block = ts.get_node_text(match[3][1], stdout),
-                        kind = 13,
-                        range = withLocation and {
-                            start = { line = start_row, character = start_col },
-                            ["end"] = { line = end_row, character = end_col }
-                        } or nil
-                    })
-                end
-                if name == "id_name" then
-                    table.insert(css_data.id, {
-                        type = "id",
-                        label = ts.get_node_text(node, stdout),
-                        block = ts.get_node_text(match[6][1], stdout),
-                        kind = 13,
-                        range = withLocation and {
-                            start = { line = start_row, character = start_col },
-                            ["end"] = { line = end_row, character = end_col }
-                        } or nil
-                    })
-                end
-                if name == "value" then
-                    table.insert(css_data.imports, ts.get_node_text(node, stdout))
-                end
-            end
+    local seen_classes = {}
+    local seen_ids = {}
+
+    for id, node, _ in query:iter_captures(root, stdout, 0, -1) do
+        local name = query.captures[id]
+        local start_row, start_col, end_row, end_col = node:range()
+        local text = ts.get_node_text(node, stdout)
+
+        if name == "class_name" and not seen_classes[text] then
+            seen_classes[text] = true
+            table.insert(css_data.class, {
+                type = "class",
+                label = text,
+                block = get_rule_block_text(node, stdout),
+                kind = 13,
+                range = withLocation and {
+                    start = { line = start_row, character = start_col },
+                    ["end"] = { line = end_row, character = end_col }
+                } or nil
+            })
+        end
+
+        if name == "id_name" and not seen_ids[text] then
+            seen_ids[text] = true
+            table.insert(css_data.id, {
+                type = "id",
+                label = text,
+                block = get_rule_block_text(node, stdout),
+                kind = 13,
+                range = withLocation and {
+                    start = { line = start_row, character = start_col },
+                    ["end"] = { line = end_row, character = end_col }
+                } or nil
+            })
+        end
+
+        if name == "value" then
+            table.insert(css_data.imports, text)
         end
     end
 
